f252d186c21e6df80389c365db83d3b8d38131a5
Song-Hyun-Jung <guswjd661207@naver.com>
2022-06-06 13:58:39 +0900

    validator 주석 추가

--- SSD/src/main/java/com/example/demo/service/UpdateUserValidator.java
+++ SSD/src/main/java/com/example/demo/service/UpdateUserValidator.java
@@ -18,13 +18,15 @@ public class UpdateUserValidator implements Validator{
 		UpdateUserCommand updateUserCommand = (UpdateUserCommand)obj;
 		System.out.println("validator로 넘어옴");
 		
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "required", "비밀번호는 필수입니다.");
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "nickname", "required", "닉네임은 필수입니다.");
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "phone", "required", "번호는 필수입니다.");
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "address", "required", "주소는 필수입니다.");
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "payment", "required", "결제정보는 필수입니다.");
+		//회원정보 수정 validate
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "required", "비밀번호는 필수입니다."); //비밀번호 확인
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "nickname", "required", "닉네임은 필수입니다.");//닉네임 확인
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "phone", "required", "번호는 필수입니다.");//전화번호 입력 안했을 경우
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "address", "required", "주소는 필수입니다.");//주소 확인
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "payment", "required", "결제정보는 필수입니다.");//결제정보 확인
 		
 		
+		//전화번호 형식 확인
 		String phone = updateUserCommand.getPhone();
 		if (!phone.equals("") && !phone.matches("^[0][1]\\d{1}-\\d{3,4}-\\d{4}$")) {
 				errors.rejectValue("phone", "typeMismatch"); // 01x-xxx-xxxx or 01x-xxxx-xxxx인지 검증
